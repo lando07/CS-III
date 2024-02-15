60f514f45fb398e8d5ffdf68c3c04194cdd31b6c
lando07 <96402540+lando07@users.noreply.github.com>
2024-02-15 12:24:23 -0600

    finished browser history

---
+++ Unit 1/browserHistory.cpp
@@ -0,0 +1,57 @@
+#include "util.h"
+#include <stack>
+#include <string>
+
+void goBack();
+void goToNewSite();
+void goForward();
+
+stack<string> historyForward;
+stack<string> historyBack;
+
+int main() {
+  // Enter your code here
+  while (true) {
+    int ans = readInt(1, 4,
+                      "Would you like to:\n1 - Go to a new site\n2 - Go "
+                      "Back\n3 - Go Forward\n4 - Exit\n>> ",
+                      ">> ");
+    switch (ans) {
+    case 1:
+      goToNewSite();
+      break;
+    case 2:
+      goBack();
+      break;
+    case 3:
+      goForward();
+      break;
+    case 4:
+      return 0;
+    }
+    cout << "You are currently browsing " << historyBack.top() << endl << endl;
+  }
+  return 0;
+}
+
+void goToNewSite() {
+  historyForward = stack<string>();
+  string ans = readLine("Enter the new site to browse: ");
+  historyBack.push(ans);
+}
+
+void goBack() {
+  string site = historyBack.top();
+  historyBack.pop();
+  historyForward.push(site);
+}
+
+void goForward() {
+  if (historyForward.empty()) {
+    cout << "You cannot go forward." << endl;
+  } else {
+    string site = historyForward.top();
+    historyForward.pop();
+    historyBack.push(site);
+  }
+}
