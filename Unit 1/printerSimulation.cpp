7fdac4332631b3bae28bbfaded5b0a3c51122a46
lando07 <96402540+lando07@users.noreply.github.com>
2024-02-21 12:40:52 -0600

    rpn calc ez

---
+++ Unit 1/printerSimulation.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <queue>
+#include "util.h"
+
+int main(){
+    
+}
--- Unit 1/rpnCalculator.cpp
+++ Unit 1/rpnCalculator.cpp
@@ -1,9 +1,50 @@
 #include "util.h"
+#include <iostream>
 #include <stack>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
 int main() {
   // Enter your code here
-
+  stack<int> operands;
+  while (true) {
+    string ans = readLine("> ");
+    if (ans == "exit") {
+      return 0;
+    }
+    try {
+      int num = stoi(ans);
+      operands.push(num);
+    } catch (invalid_argument) {
+      char c = ans[0];
+      if (operands.size() >= 2) {
+        int second = operands.top();
+        operands.pop();
+        int first = operands.top();
+        operands.pop();
+        switch (c) {
+        case '+':
+          cout << first + second << endl;
+          operands.push(first + second);
+          break;
+        case '-':
+          cout << first - second << endl;
+          operands.push(first - second);
+          break;
+        case '/':
+          cout << first / second << endl;
+          operands.push(first / second);
+          break;
+        case '*':
+          cout << first * second << endl;
+          operands.push(first * second);
+          break;
+        }
+      } else {
+        cout << "Error - Not enough numbers in stack" << endl;
+      }
+    }
+  }
   return 0;
 }
