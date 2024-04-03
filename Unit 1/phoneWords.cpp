160085b29ca0e03062bcfdfcb6a5e04b47d015f7
lando07 <96402540+lando07@users.noreply.github.com>
2024-04-02 19:33:08 -0500

    phoneWords done

---
+++ Unit 1/phoneWords.cpp
@@ -0,0 +1,56 @@
+#include "util.h"
+#include <fstream>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+set<string> words;
+
+vector<vector<char>> letters{{},
+                             {},
+                             {'A', 'B', 'C'},
+                             {'D', 'E', 'F'},
+                             {'G', 'H', 'I'},
+                             {'J', 'K', 'L'},
+                             {'M', 'N', 'O'},
+                             {'P', 'Q', 'R', 'S'},
+                             {'T', 'U', 'V'},
+                             {'W', 'X', 'Y', 'Z'}};
+
+void loadDict() {
+  ifstream in;
+  in.open("words.txt");
+  string word;
+  while (true) {
+    getline(in, word);
+    if (in.fail())
+      break;
+    words.insert(word);
+  }
+}
+
+// depth first search
+void searchPNumber(string number, string word) {
+  if (number.size() == 0 || word.size() > 7) {
+    if (words.find(word) != words.end()) {
+      cout << word << endl;
+    }
+    return;
+  }
+  // cout << "Number parsed: " << number.substr(0, 1) << endl;
+  // cout << "Word: " << word << endl;
+  for (char c : letters[stoi(number.substr(0, 1))]) {
+    string newWord = word + c;
+    searchPNumber(number.substr(1), newWord);
+  }
+}
+
+int main() {
+  loadDict();
+  string number = readLine("Please enter a phone number: ");
+  searchPNumber(number, "");
+  cout << "End of List" << endl;
+
+  return 0;
+}
