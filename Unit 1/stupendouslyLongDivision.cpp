c85d1ecc39ff9560e4b62f476578b48aad4e2c67
lando07 <96402540+lando07@users.noreply.github.com>
2024-04-23 12:40:22 -0500

    oh lawd

--- Unit 1/stupendouslyLongDivision.cpp
+++ Unit 1/stupendouslyLongDivision.cpp
@@ -1,4 +1,5 @@
 #include "util.h"
+#include <cstddef>
 #include <string>
 
 struct digit {
@@ -8,32 +9,46 @@ struct digit {
 
 void gigantimasaurousLongDivision(digit *head, bool leading, int carry,
                                   int divisor) {
-  if (divisor > head->num) {
-    if (!leading) {
-      cout << ((head->num + carry) / divisor);
-    } else {
-      cout << 0;
-    }
-    gigantimasaurousLongDivision(head->next, false,
-                                 ((head->num + carry) % divisor) * 10, divisor);
+  if (head == NULL) {
+    cout << endl;
   } else {
-    cout << ((head->num + carry) / divisor);
-    gigantimasaurousLongDivision(head->next, false,
-                                 ((head->num + carry) % divisor) * 10, divisor);
+    int tmpDividend = carry + head->num;
+    if (divisor > tmpDividend) {
+      if (!leading) {
+        cout << 0;
+      }
+      gigantimasaurousLongDivision(head->next, false, tmpDividend * 10,
+                                   divisor);
+    } else {
+      cout << tmpDividend / divisor;
+      gigantimasaurousLongDivision(head->next, false,
+                                   (tmpDividend % divisor) * 10, divisor);
+    }
   }
 }
 
+void printList(digit *head) {
+  if (head == NULL) {
+    cout << endl;
+    return;
+  }
+  cout << head->num;
+  printList(head->next);
+}
+
 digit *makeList(string num) {
-  digit *head = new digit{num[num.size() - 1], NULL};
-  for (int i = num.size() - 2; i > -1; i++) {
-    digit *newHead = new digit{num[i], head};
+  digit *head = new digit{num[num.size() - 1] - '0', NULL};
+  for (int i = num.size() - 2; i > -1; i--) {
+    digit *newHead = new digit{num[i] - '0', head};
     head = newHead;
+    // cout << head->num << endl;
   }
   return head;
 }
 
 void gigaDivide(string dividend, int divisor) {
   digit *head = makeList(dividend);
+  //printList(head);
   gigantimasaurousLongDivision(head, true, 0, divisor);
 }
 
@@ -42,7 +57,5 @@ int main() {
       "Please enter a gigantamasaurous heckin mega chonker dividend: ");
   int divisor = readInt("Please enter an integer divisor: ");
   cout << "Solution: ";
-  digit* ptr = new digit;
   gigaDivide(dividend, divisor);
-  delete ptr;
 }
