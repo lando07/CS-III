b9ce2841c2b68532e5e2ad17843b5e1929bfdb24
Landon Smith <96402540+lando07@users.noreply.github.com>
2024-04-24 14:45:16 +0000

    kept working on playlist

--- Unit 1/playlist.cpp
+++ Unit 1/playlist.cpp
@@ -6,15 +6,31 @@ struct song {
   song *next;
 };
 
+void addSong(song* head, string songName){
+
+}
+
+void printPrev(song* head){
+
+}
+
+void printNext(song* head){
+
+}
 int main() {
+  song* head = new song{"",NULL,NULL};
+  song* tail = head;
   while (true) {
     int ans =
         readInt("Please select an option:\n1 - Print the previous song\n2 - "
                 "Add a new song\n3 - Print the next song\n4 - Quit\n>>");
     switch (ans) {
     case 1:
+    printPrev(head);
     case 2:
+    addSong(head, readLine("What's the song name? >> "));
     case 3:
+    printNext(head);
     case 4:
       return 0;
     }
