736a00f4ff811e2055a61a5846fe76d754700270
lando07 <96402540+lando07@users.noreply.github.com>
2024-04-24 13:17:46 -0500

    okeh done

--- Unit 1/playlist.cpp
+++ Unit 1/playlist.cpp
@@ -1,4 +1,5 @@
 #include "util.h"
+#include <cstddef>
 // we emulatin a doubly linked list
 struct song {
   string name;
@@ -6,27 +7,57 @@ struct song {
   song *next;
 };
 
-void addSong(song *head, string songName) {}
+void addSong(song *&head, string songName) {
+  if (head->name == "") {
+    head->name = songName;
+    return;
+  }
+  song *newSong = new song{songName, head, NULL};
+  head->next = newSong;
+  head = newSong;
+}
 
-void printPrev(song *head) {}
+void printPrev(song *&currSong) {
+  if (currSong->name == "") {
+    cout << "Your playlist is empty." << endl;
+  } else if (currSong->prev == NULL) {
+    cout << "No previous song has played" << endl;
+  } else {
+    cout << currSong->prev->name << endl;
+    currSong = currSong->prev;
+  }
+}
 
-void printNext(song *head) {}
+void printNext(song *&currSong) {
+  if (currSong->name == "") {
+    cout << "Your playlist is empty." << endl;
+  } else if (currSong->next == NULL) {
+    cout << "No more songs are in the queue" << endl;
+  } else {
+    cout << currSong->next->name << endl;
+    currSong = currSong->next;
+  }
+}
 int main() {
   song *head = new song{"", NULL, NULL};
-  song *tail = head;
+  song *currSong = head;
   while (true) {
     int ans =
-        readInt("Please select an option:\n1 - Print the previous song\n2 - "
+        readInt("\nPlease select an option:\n1 - Print the previous song\n2 - "
                 "Add a new song\n3 - Print the next song\n4 - Quit\n>>");
     switch (ans) {
     case 1:
-      printPrev(head);
+      printPrev(currSong);
+      break;
     case 2:
       addSong(head, readLine("What's the song name? >> "));
+      break;
     case 3:
-      printNext(head);
+      printNext(currSong);
+      break;
     case 4:
       return 0;
     }
+    cout << "Current song: " << currSong->name << endl;
   }
 }
